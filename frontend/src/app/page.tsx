d2ccbf392d7b08de7632eb03be9f714df96b4df5
M-Ahmed28 <muhammadahmed.softeng@gmail.com>
2025-10-20 15:45:18 +0500

    initail commit with MUI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Welcome to PakSentiment</h1>
+      <h1>Welcome to PakSentiment Frontend</h1>
       <p>Your go-to platform for sentiment analysis in Pakistan.</p>
     </div>
   );
