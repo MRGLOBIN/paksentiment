2d383e4458d8ca618044c21e85979b564bbf7341
M-Ahmed28 <muhammadahmed.softeng@gmail.com>
2025-10-20 15:42:43 +0500

    initail commit with MUI

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to PakSentiment</h1>
+      <p>Your go-to platform for sentiment analysis in Pakistan.</p>
+    </div>
+  );
+}
